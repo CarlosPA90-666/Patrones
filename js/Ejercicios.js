0c0c8796b64f29a82e374bb63f2bd05892177252
carlos plaza <carlos.plaza01@correo.usa.edu.co>
2021-03-19 16:30:09 -0500

    Primer avance Jquery

---
+++ js/Ejercicios.js
@@ -0,0 +1,11 @@
+function mostrarbrujula(){
+
+    document.getElementById('Ejercicios-subtitulos').style.display="none";
+    document.getElementById('regresardefisicas').style.display="none";
+    console.log('aaa')
+    
+}
+
+$("#brujula").click(function(){
+	mostrarbrujula();
+});
