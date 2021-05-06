9cdedb72b27f861f9b338283b0601dcb551d78e0
Kyratyx <70303578+Kyratyx@users.noreply.github.com>
2021-05-06 10:04:43 -0500

    Schema.py

--- patrones/schema.py
+++ patrones/schema.py
@@ -4,6 +4,8 @@ instructions = [
     'DROP TABLE IF EXISTS Recordatorio;',
     'DROP TABLE IF EXISTS Medicamento;',
     'DROP TABLE IF EXISTS Cita;',
+    'DROP TABLE IF EXISTS Familia;',
+    'DROP TABLE IF EXISTS Mensaje;',
     'SET FOREIGN_KEY_CHECKS=1;',
     """
         CREATE TABLE Usuario(
@@ -14,7 +16,8 @@ instructions = [
             address VARCHAR(100) NOT NULL,
             phone VARCHAR(10) NOT NULL,
             birthdate DATE NOT NULL,
-            role INT NOT NULL
+            role INT NOT NULL,
+            family VARCHAR(100) NOT NULL
         );
     """,
     """
@@ -38,6 +41,24 @@ instructions = [
             price DOUBLE NOT NULL,
             amount INT NOT NULL,
             FOREIGN KEY (created_by) REFERENCES Usuario (id)
+        );
+    """,
+    """
+        CREATE TABLE Familia(
+            created_by INT NOT NULL,
+            family VARCHAR(100) primary key NOT NULL,
+            FOREIGN KEY (created_by) REFERENCES Usuario (id),
+            FOREIGN KEY (family) REFERENCES Usuario(username)
+        );
+    """,
+    """
+        CREATE TABLE Mensaje(
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            created_by INT NOT NULL,
+            family_Initials VARCHAR(100) NOT NULL,
+            Contentmsg VARCHAR(100) NOT NULL,
+            FOREIGN KEY (created_by) REFERENCES Usuario (id),
+            FOREIGN KEY (family_Initials) REFERENCES Familia(family)
         );
     """,
     """
